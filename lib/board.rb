7c92f138dedda029e8f5e07927e33d14bde2350c
devinlightfoot <euler2171828@gmail.com>
2020-07-21 12:26:15 -0700

    refactored Board#display

--- lib/board.rb
+++ lib/board.rb
@@ -1,36 +1,62 @@
-require_relative 'pieces'
+#require_relative 'pieces'
 module UC
-    White_king ||= "♔"
-    White_queen ||= "♕"
-    White_rook ||= "♖"
-    White_bishop ||= "♗"
-    White_knight ||= "♘"
-    White_pawn ||= "♙"
-    Black_king ||= "♚"
-    Black_queen ||= "♛"
-    Black_rook ||= "♜"
-    Black_bishop ||= "♝"
-    Black_knight ||= "♞"
-    Black_pawn ||= "♟"
-    space = "\u0020"
-    Space ||= space.encode('utf-8')
+  White_king ||= '♔'.freeze
+  White_queen ||= '♕'.freeze
+  White_rook ||= '♖'.freeze
+  White_bishop ||= '♗'.freeze
+  White_knight ||= '♘'.freeze
+  White_pawn ||= '♙'.freeze
+  Black_king ||= '♚'.freeze
+  Black_queen ||= '♛'.freeze
+  Black_rook ||= '♜'.freeze
+  Black_bishop ||= '♝'.freeze
+  Black_knight ||= '♞'.freeze
+  Black_pawn ||= '♟'.freeze
+  space = "\u0020"
+  Space ||= space.encode('utf-8')
 end
-class Board
+
+class Square
     include UC
-    attr_accessor :board
-    def initialize()
-        @board = Array.new(8) { Array.new(8,Space) }
+    attr_accessor :pos, :piece, :symbol
+    def initialize(pos, piece = nil)
+        @pos = pos
+        @row = pos[0]
+        @col = pos[1]
+        @piece = piece
+        @symbol = Space
     end
-    def display
-        @board.each { |col|
-          print "\n"
-          @s = "+----+----+----+----+----+----+----+----+"
-          puts @s
-          col.each {|row|
-            print "| #{row}  "
-            } 
-            print "|"}
-            print "\n"
-            print @s
-      end
+    def print_sq
+        @piece.nil? ? @symbol = Space : @symbol = @piece.symbol
+        return @symbol
+    end
 end
+
+class Board
+  include UC
+  attr_accessor :board
+  def initialize
+    @board = Array.new(8) { Array.new(8, Space) }
+    @board.each_with_index do |row, ir|
+        row.each_with_index do |col, ic|
+            @board[ir][ic] = Square.new([ir, ic])
+        end
+    end
+  end
+
+  def display
+    @board.each do |col|
+      print "\n"
+      @s = '+----+----+----+----+----+----+----+----+'
+      puts @s
+      col.each do |row|
+        print "| #{row.print_sq}  "
+      end
+      print '|'
+    end
+    print "\n"
+    print @s
+    end
+end
+b = Board.new
+b.display
